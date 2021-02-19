993c475c49ceb355d8d1ed2c19131d0b8901710b
Michal Kreft <michal.kreft@softwire.com>
2021-02-19 14:09:41 +0000

    Update order to include multiple strands

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -2,19 +2,23 @@
 
 module OrderHelper
   def elements_in_order(elements:, previous_method_name:)
-    first_element = elements.find { |element| !elements.include?(element.send(previous_method_name)) }
-    return [] unless first_element
+    first_elements = elements.filter { |element| !elements.include?(element.send(previous_method_name)) }
+    return [] unless first_elements.any?
 
-    ordered_elements = [first_element]
-    current_element = first_element
-    next_element = elements.find { |element| element.send(previous_method_name) == current_element }
-
-    while elements.include?(next_element)
-      ordered_elements += [next_element]
-      current_element = next_element
+    strands = first_elements.map do |first_element|
+      ordered_elements = [first_element]
+      current_element = first_element
       next_element = elements.find { |element| element.send(previous_method_name) == current_element }
+
+      while elements.include?(next_element)
+        ordered_elements += [next_element]
+        current_element = next_element
+        next_element = elements.find { |element| element.send(previous_method_name) == current_element }
+      end
+
+      ordered_elements
     end
 
-    ordered_elements
+    strands.flatten
   end
 end
