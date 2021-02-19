dad320e8c99ce83b83233e1549994b219a68e4fb
Michal Kreft <michal.kreft@softwire.com>
2021-02-19 09:31:17 +0000

    Update core_induction_programme_exporter.rb

--- app/models/core_induction_programme_exporter.rb
+++ app/models/core_induction_programme_exporter.rb
@@ -9,45 +9,36 @@ class CoreInductionProgrammeExporter
       import: true,
     )
 
-    course_years = CourseYear.order(:created_at)
+    years = CourseYear.order(:created_at)
     SeedDump.dump(
-      course_years,
+      years,
       file: "db/seeds/cip_seed_dump.rb",
       exclude: %i[created_at updated_at],
       import: true,
       append: true,
     )
 
-    course_modules = []
-    course_years.each do |course_year|
-      course_modules += course_year.course_modules.order(:created_at)
-    end
+    modules = years.map { |course_year| course_year.course_modules.order(:created_at) }.flatten
     SeedDump.dump(
-      course_modules,
+      modules,
       file: "db/seeds/cip_seed_dump.rb",
       exclude: %i[created_at updated_at],
       import: true,
       append: true,
     )
 
-    course_lessons = []
-    course_modules.each do |course_module|
-      course_lessons += course_module.course_lessons.order(:created_at)
-    end
+    lessons = modules.map { |course_module| course_module.course_lessons.order(:created_at) }.flatten
     SeedDump.dump(
-      course_lessons,
+      lessons,
       file: "db/seeds/cip_seed_dump.rb",
       exclude: %i[created_at updated_at],
       import: true,
       append: true,
     )
 
-    course_lesson_parts = []
-    course_lessons.each do |course_lesson|
-      course_lesson_parts += course_lesson.course_lesson_parts.order(:created_at)
-    end
+    parts = lessons.map { |course_lesson| course_lesson.course_lesson_parts.order(:created_at) }.flatten
     SeedDump.dump(
-      course_lesson_parts,
+      parts,
       file: "db/seeds/cip_seed_dump.rb",
       exclude: %i[created_at updated_at],
       import: true,
