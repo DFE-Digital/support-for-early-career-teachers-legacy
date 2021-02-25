b275aaffd5fbb014b97675b8a0a94625e438a9a2
Steven <steven.woods@digital.education.gov.uk>
2021-02-25 12:15:13 +0000

    update specs for testing course duration

--- spec/models/course_lesson_spec.rb
+++ spec/models/course_lesson_spec.rb
@@ -8,6 +8,7 @@ RSpec.describe CourseLesson, type: :model do
       CourseLesson.create(
         title: "Test Course lesson",
         course_module: FactoryBot.create(:course_module),
+        completion_time_in_minutes: 10,
       )
     }.to change { CourseLesson.count }.by(1)
   end
@@ -23,5 +24,6 @@ RSpec.describe CourseLesson, type: :model do
     subject { FactoryBot.create(:course_lesson) }
     it { is_expected.to validate_presence_of(:title).with_message("Enter a title") }
     it { is_expected.to validate_length_of(:title).is_at_most(255) }
+    it { is_expected.to validate_numericality_of(:completion_time_in_minutes).is_greater_than_or_equal_to(1).with_message("Enter a number greater than 0") }
   end
 end
