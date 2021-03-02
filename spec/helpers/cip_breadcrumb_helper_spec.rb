8cdd5e47c6eca757df17dae3628c3284ddee2a40
Steven <steven.woods@digital.education.gov.uk>
2021-03-02 20:47:40 +0000

    additional tests for each breadcrumb and refactored.

--- spec/helpers/cip_breadcrumb_helper_spec.rb
+++ spec/helpers/cip_breadcrumb_helper_spec.rb
@@ -2,56 +2,69 @@
 
 require "rails_helper"
 
-describe CipBreadcrumbHelper do
-  describe "core-induction-programme" do
-    let(:user) { create(:user, :admin) }
-    let(:course_year) { create(:course_year) }
-    let(:course_module) { create(:course_module, course_year: course_year) }
-    let(:course_lesson) { create(:course_lesson, course_module: course_module) }
+describe CipBreadcrumbHelper, type: :helper do
+  before :each do
+    @user = FactoryBot.create(:user, :admin)
+    @course_year = FactoryBot.create(:course_year)
+    @course_year_crumb = [@course_year.title, "/core-induction-programme/years/#{@course_year.id}"]
+  end
 
+  describe "#year_breadcrumb" do
+    let(:course_year_breadcrumb) { helper.course_year_breadcrumbs(@user, @course_year) }
     it "returns an array for the course year breadcrumb" do
-      course_year_breadcrumb = helper.course_year_breadcrumbs(user, course_year)
-
-      course_year_crumb = [["Home", "/dashboard"], [course_year.title, "/core-induction-programme/years/#{course_year.id}"]]
-
-      expect(course_year_breadcrumb).to eq(course_year_crumb)
+      expect(course_year_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb])
     end
+
+    it "returns a url for the end crumb when the action_name is edit" do
+      allow(helper).to receive(:action_name) { "edit" }
+      expect(course_year_breadcrumb).to eql([["Home", "/dashboard"], @course_year_crumb])
+    end
+
+    it "returns just the title for the end crumb when the action_name is show" do
+      allow(helper).to receive(:action_name) { "show" }
+      expect(course_year_breadcrumb).to eql([["Home", "/dashboard"], @course_year.title])
+    end
+  end
+
+  describe "#module_breadcrumb" do
+    let(:course_module) { create(:course_module, course_year: @course_year) }
+    let(:course_module_crumb) { [course_module.title, "/core-induction-programme/years/#{@course_year.id}/modules/#{course_module.id}"] }
+    let(:course_module_breadcrumb) { helper.course_module_breadcrumbs(@user, course_module) }
 
     it "returns an array for the course module breadcrumb" do
-      course_module_breadcrumb = helper.course_module_breadcrumbs(user, course_module)
-
-      course_year_crumb = [course_year.title, "/core-induction-programme/years/#{course_year.id}"]
-      course_module_crumb = [course_module.title, "/core-induction-programme/years/#{course_year.id}/modules/#{course_module.id}"]
-
-      expect(course_module_breadcrumb).to eq([["Home", "/dashboard"], course_year_crumb, course_module_crumb])
+      expect(course_module_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb])
     end
+
+    it "returns a url for the end crumb when the action_name is edit" do
+      allow(helper).to receive(:action_name) { "edit" }
+      expect(course_module_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb])
+    end
+
+    it "returns just the title for the end crumb when the action_name is show" do
+      allow(helper).to receive(:action_name) { "show" }
+      expect(course_module_breadcrumb).to eql([["Home", "/dashboard"], @course_year_crumb, course_module.title])
+    end
+  end
+
+  describe "#lesson_breadcrumb" do
+    let(:course_module) { create(:course_module, course_year: @course_year) }
+    let(:course_lesson) { create(:course_lesson, course_module: course_module) }
+    let(:course_module_crumb) { [course_module.title, "/core-induction-programme/years/#{@course_year.id}/modules/#{course_module.id}"] }
+    let(:course_lesson_crumb) { [course_lesson.title, "/core-induction-programme/years/#{@course_year.id}/modules/#{course_module.id}/lessons/#{course_lesson.id}"] }
+    let(:course_lesson_breadcrumb) { helper.course_lesson_breadcrumbs(@user, course_lesson) }
 
     it "returns an array for the course lesson breadcrumb" do
-      course_lesson_breadcrumb = helper.course_lesson_breadcrumbs(user, course_lesson)
-
-      course_year_crumb = [course_year.title, "/core-induction-programme/years/#{course_year.id}"]
-      course_module_crumb = [course_module.title, "/core-induction-programme/years/#{course_year.id}/modules/#{course_module.id}"]
-      course_lesson_crumb = [course_lesson.title, "/core-induction-programme/years/#{course_year.id}/modules/#{course_module.id}/lessons/#{course_lesson.id}"]
-
-      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], course_year_crumb, course_module_crumb, course_lesson_crumb])
+      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb, course_lesson_crumb])
     end
 
-    it "returns a string for the end crumb when rendered the action_name is show" do
-      allow(helper).to receive(:action_name) { "show" }
-
-      course_year_breadcrumb = helper.course_year_breadcrumbs(user, course_year)
-      course_year_crumb = [["Home", "/dashboard"], "Test Course year"]
-
-      expect(course_year_breadcrumb).to eql(course_year_crumb)
-    end
-
-    it "returns the title and url for the end crumb when the action_name is edit" do
+    it "returns the url for the end crumb when the action_name is edit" do
       allow(helper).to receive(:action_name) { "edit" }
+      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb, course_lesson_crumb])
+    end
 
-      course_year_breadcrumb = helper.course_year_breadcrumbs(user, course_year)
-      course_year_crumb = [["Home", "/dashboard"], [course_year.title, "/core-induction-programme/years/#{course_year.id}"]]
-
-      expect(course_year_breadcrumb).to eql(course_year_crumb)
+    it "returns just the title for the end crumb when the action_name is show" do
+      allow(helper).to receive(:action_name) { "show" }
+      expect(course_lesson_breadcrumb).to eq([["Home", "/dashboard"], @course_year_crumb, course_module_crumb, course_lesson.title])
     end
   end
 end
