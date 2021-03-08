aba216725234758a549eb4b95b5e1f41d847955d
Steven <steven.woods@digital.education.gov.uk>
2021-03-08 16:25:18 +0000

    update course lesson duration human readable time

--- app/models/course_lesson.rb
+++ app/models/course_lesson.rb
@@ -24,16 +24,12 @@ class CourseLesson < ApplicationRecord
   end
 
   def duration_in_minutes_in_words
-    number_of_hours = completion_time_in_minutes / 60
-    number_of_minutes = completion_time_in_minutes % 60
+    hours = completion_time_in_minutes / 60
+    minutes = completion_time_in_minutes % 60
 
-    hour_string = "hour".pluralize(number_of_hours)
-    minute_string = "minute".pluralize(number_of_minutes)
-
-    if number_of_hours.positive?
-      "#{number_of_hours} #{hour_string} #{number_of_minutes} #{minute_string}"
-    else
-      "#{number_of_minutes} #{minute_string}"
-    end
+    [].tap { |lesson_duration_string|
+      lesson_duration_string << "#{hours} hour".pluralize(hours)       unless hours.zero?
+      lesson_duration_string << "#{minutes} minute".pluralize(minutes) unless minutes.zero?
+    }.join(" ")
   end
 end
