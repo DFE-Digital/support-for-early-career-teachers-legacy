cc2a79eaa9afb2427a2fb0e5d3a5d12f66fdd532
Steven <steven.woods@digital.education.gov.uk>
2021-03-09 09:17:11 +0000

    refactor duration_in_minutes_in_words method to use if else block

--- app/models/course_lesson.rb
+++ app/models/course_lesson.rb
@@ -24,12 +24,18 @@ class CourseLesson < ApplicationRecord
   end
 
   def duration_in_minutes_in_words
-    hours = completion_time_in_minutes / 60
-    minutes = completion_time_in_minutes % 60
+    number_of_hours = completion_time_in_minutes / 60
+    number_of_minutes = completion_time_in_minutes % 60
 
-    [].tap { |lesson_duration_string|
-      lesson_duration_string << "#{hours} hour".pluralize(hours)       unless hours.zero?
-      lesson_duration_string << "#{minutes} minute".pluralize(minutes) unless minutes.zero?
-    }.join(" ")
+    hour_string = "hour".pluralize(number_of_hours)
+    minute_string = "minute".pluralize(number_of_minutes)
+
+    if number_of_hours.positive? && number_of_minutes.positive?
+      "#{number_of_hours} #{hour_string} #{number_of_minutes} #{minute_string}"
+    elsif number_of_hours.positive?
+      "#{number_of_hours} #{hour_string}"
+    else
+      "#{number_of_minutes} #{minute_string}"
+    end
   end
 end
