d92484b9158692d9045107de5c733225fd8d6a88
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-03-09 12:35:19 +0000

    Add cookie page and banner

--- app/webpacker/packs/application.js
+++ app/webpacker/packs/application.js
@@ -4,5 +4,6 @@ require.context("govuk-frontend/govuk/assets");
 import "../styles/application.scss";
 import { initAll } from "govuk-frontend";
 import "./admin/supplier-users";
+import "./cookie-banner";
 
 initAll();
