870a52dfcded4fa645dcea1ab187b75a78056039
Steven <steven.woods@digital.education.gov.uk>
2021-04-07 20:55:59 +0100

    Add migration to remove cip id from course_years

--- db/migrate/20210406152016_remove_core_induction_programme_id_from_course_years.rb
+++ db/migrate/20210406152016_remove_core_induction_programme_id_from_course_years.rb
@@ -2,6 +2,6 @@
 
 class RemoveCoreInductionProgrammeIdFromCourseYears < ActiveRecord::Migration[6.1]
   def change
-    remove_column :course_years,  :core_induction_programme_id,  :uuid
+    remove_column :course_years, :core_induction_programme_id, :uuid
   end
 end
