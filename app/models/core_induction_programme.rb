afd1c897cadef40b6423312a0073ba7976496ea9
Steven <steven.woods@digital.education.gov.uk>
2021-04-07 20:56:47 +0100

    Add course_years method to cip model

--- app/models/core_induction_programme.rb
+++ app/models/core_induction_programme.rb
@@ -6,4 +6,8 @@ class CoreInductionProgramme < ApplicationRecord
   belongs_to :course_year_two, class_name: "CourseYear", optional: true
   has_many :early_career_teacher_profiles
   has_many :early_career_teachers, through: :early_career_teacher_profiles, source: :user
+
+  def course_years
+    [course_year_one, course_year_two].filter(&:present?)
+  end
 end
