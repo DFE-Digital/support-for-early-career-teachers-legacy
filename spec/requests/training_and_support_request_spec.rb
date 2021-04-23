01b6741a5e77b81ce0178b7fbc3b4a10e6de92dd
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-04-23 15:03:44 +0100

    Fix rspec

--- spec/requests/training_and_support_request_spec.rb
+++ spec/requests/training_and_support_request_spec.rb
@@ -4,10 +4,15 @@ require "rails_helper"
 
 RSpec.describe "TrainingAndSupports", type: :request do
   describe "GET /show" do
+    before do
+      cip = create(:core_induction_programme)
+      early_career_teacher = create(:user, :early_career_teacher, { core_induction_programme: cip })
+      sign_in early_career_teacher
+    end
+
     it "returns http success" do
       get "/training-and-support"
-      expect(response).to have_http_status(:success)
-      expect(response).to render(:show)
+      expect(response).to render_template(:show)
     end
   end
 end
