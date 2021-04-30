306b3dca41b97d30bd40c7102b22990352840099
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-04-30 10:15:09 +0100

    Better errors when login fails (#83)
    * Better errors when login fails
    
    * Use Devise regex for email validation

--- lib/devise/strategies/yolo_authenticatable.rb
+++ lib/devise/strategies/yolo_authenticatable.rb
@@ -5,14 +5,36 @@ require "devise/strategies/authenticatable"
 module Devise
   module Strategies
     class YoloAuthenticatable < Authenticatable
+      # Validation is handled by authenticate! below so that errors look nicer
+      def valid?
+        true
+      end
+
       def authenticate!
         if params[:user].present?
-          user = User.find_by(email: params[:user][:email])
+          email = params[:user][:email]
+
+          if email == ""
+            user = User.new
+            user.errors.add :email, "Enter an email address"
+            success! user
+            return
+          end
+
+          unless email.match(Devise.email_regexp)
+            user = User.new
+            user.errors.add :email, "Enter an email address in the correct format, like name@school.org"
+            success! user
+            return
+          end
+
+          user = User.find_by(email: email)
 
           if user.blank?
             user = User.new
             user.errors.add :email, "Enter the email address your school used when they registered your account"
           end
+
           success! user
         end
       end
