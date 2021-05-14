2b857c5c0d011507b957468df1e62e1faad1aebb
Michal Kreft <michal.kreft@softwire.com>
2021-05-14 16:48:38 +0100

    CPDEL-383: Update synching users

--- app/services/register_and_partner_api/sync_users.rb
+++ app/services/register_and_partner_api/sync_users.rb
@@ -4,30 +4,43 @@ module RegisterAndPartnerApi
   class SyncUsers
     def self.perform
       # TODO: Add the last time we synced users, to avoid getting too many of them back
+      # TODO: Add pagination
       begin
         sync_users(RegisterAndPartnerApi::User.all)
       rescue JsonApiClient::Errors::ClientError
         # This is how the API responds when we run out of pages :/
       end
-
-      # TODO: Make sure it works
-      # RegisterAndPartnerApi::SyncCheck.set_last_sync(Time.zone.now)
     rescue JsonApiClient::Errors::ApiError
-      # TODO: Once the API is set up and working on R&P, uncomment that line
-      # raise RegisterAndPartnerApi::SyncError
+      raise RegisterAndPartnerApi::SyncError
     end
 
     def self.sync_users(users)
+      logger = Rails.logger
       users.each do |remote_user|
-        attributes = remote_user.attributes
-        user = ::User.find_or_initialize_by(id: attributes[:id]) do |u|
-          u.full_name = attributes[:full_name]
-          u.email = attributes[:email]
-        end
+        attributes = user_attributes_from(remote_user)
+
+        user = ::User.find_or_initialize_by(email: attributes[:email])
+        user.full_name = attributes[:full_name]
+
+        ect_profile = ::EarlyCareerTeacherProfile.find_or_initialize_by(user: user)
+        ect_profile.core_induction_programme = CoreInductionProgramme.find_by(name: attributes[:cip])
+
+        ect_profile.save!
         user.save!
+      rescue StandardError
+        logger.warn("Error saving user!")
       end
     end
 
-    private_class_method :sync_users
+    def self.user_attributes_from(user_from_api)
+      {
+        register_and_partner_id: user_from_api.attributes[:id],
+        full_name: user_from_api.attributes[:attributes][:full_name],
+        email: user_from_api.attributes[:attributes][:email],
+        cip: "UCL",
+      }
+    end
+
+    private_class_method :sync_users, :user_attributes_from
   end
 end
