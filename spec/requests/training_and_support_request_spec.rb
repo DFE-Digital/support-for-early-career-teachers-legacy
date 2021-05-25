b1e6be81078494ee51322e5f465956e8dd43af27
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-05-25 16:52:14 +0100

    PR feedback + test failure

--- spec/requests/training_and_support_request_spec.rb
+++ spec/requests/training_and_support_request_spec.rb
@@ -5,7 +5,9 @@ require "rails_helper"
 RSpec.describe "TrainingAndSupports", type: :request do
   describe "GET /show" do
     before do
+      cip = create(:core_induction_programme, name: "UCL")
       early_career_teacher = create(:user, :early_career_teacher)
+      early_career_teacher.early_career_teacher_profile.core_induction_programme = cip
       sign_in early_career_teacher
     end
 
