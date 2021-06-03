554107b221c0b38e84fa48b9a7af4615615a7e99
Michal Kreft <michal.kreft@softwire.com>
2021-06-03 14:52:41 +0100

    Add tests for reusing unsent emails

--- spec/services/invite_participants_spec.rb
+++ spec/services/invite_participants_spec.rb
@@ -7,17 +7,49 @@ RSpec.describe InviteParticipants do
   let(:mentor) { create(:user, :mentor) }
 
   describe "#run" do
-    it "creates an ECT email when given ECT user" do
-      expect { InviteParticipants.run([ect.email]) }.to change { InviteEmailEct.count }.by(1)
+    context "when no emails exist previously" do
+      it "creates an ECT email when given ECT user" do
+        expect { InviteParticipants.run([ect.email]) }.to change { InviteEmailEct.count }.by(1)
+      end
+
+      it "creates an mentor email when given mentor user" do
+        expect { InviteParticipants.run([mentor.email]) }.to change { InviteEmailMentor.count }.by(1)
+      end
+
+      it "creates an an ect and a mentor email when given an ect and a mentor" do
+        expect { InviteParticipants.run([ect.email, mentor.email]) }.to change { InviteEmailEct.count }.by(1)
+        expect { InviteParticipants.run([ect.email, mentor.email]) }.to change { InviteEmailMentor.count }.by(1)
+      end
     end
 
-    it "creates an mentor email when given mentor user" do
-      expect { InviteParticipants.run([mentor.email]) }.to change { InviteEmailMentor.count }.by(1)
+    context "when unsent emails exist" do
+      before do
+        InviteEmailEct.create!(user: ect)
+        InviteEmailMentor.create!(user: mentor)
+      end
+
+      it "does not create an email for ect" do
+        expect { InviteParticipants.run([ect.email, mentor.email]) }.not_to(change { InviteEmailEct.count })
+      end
+
+      it "does not create an email for mentor" do
+        expect { InviteParticipants.run([ect.email, mentor.email]) }.not_to(change { InviteEmailMentor.count })
+      end
     end
 
-    it "creates an an ect and a mentor email when given an ect and a mentor" do
-      expect { InviteParticipants.run([ect.email, mentor.email]) }.to change { InviteEmailEct.count }.by(1)
-      expect { InviteParticipants.run([ect.email, mentor.email]) }.to change { InviteEmailMentor.count }.by(1)
+    context "when sent emails exist" do
+      before do
+        InviteEmailEct.create!(user: ect, sent_at: Time.zone.now)
+        InviteEmailMentor.create!(user: mentor, sent_at: Time.zone.now)
+      end
+
+      it "creates an email for ect" do
+        expect { InviteParticipants.run([ect.email, mentor.email]) }.to change { InviteEmailEct.count }.by(1)
+      end
+
+      it "creates an email for mentor" do
+        expect { InviteParticipants.run([ect.email, mentor.email]) }.to change { InviteEmailMentor.count }.by(1)
+      end
     end
 
     it "does not stop when it gets an invalid email" do
