8185fb859cb3c91138d732ca7b535a8b9102753c
Michal Kreft <michal.kreft@softwire.com>
2021-06-03 14:04:00 +0100

    Add tests to new SyncUsers to check the email scheduling

--- spec/services/register_and_partner_api/sync_users_spec.rb
+++ spec/services/register_and_partner_api/sync_users_spec.rb
@@ -40,13 +40,30 @@ RSpec.describe RegisterAndPartnerApi::SyncUsers do
     end
 
     it "updates an existing user that has changed their email address" do
-      create(:user, register_and_partner_id: "65abf54c-c7c2-490b-9bcd-bbb4e0aab934", email: "lead-provider-1@example.com")
+      create(:user, register_and_partner_id: "65abf54c-c7c2-490b-9bcd-bbb4e0aab934", email: "mentor-1@example.com")
 
       described_class.perform
 
       record = User.find_by(register_and_partner_id: "65abf54c-c7c2-490b-9bcd-bbb4e0aab934")
       expect(User.count).to eql(3)
-      expect(record.email).to eql("lead-provider@example.com")
+      expect(record.email).to eql("mentor@example.com")
+    end
+
+    it "creates an invite email for new mentors and early career teachers" do
+      allow(InviteParticipants).to receive(:run)
+      described_class.perform
+
+      expect(InviteParticipants).to have_received(:run).with(["mentor@example.com"])
+      expect(InviteParticipants).to have_received(:run).with(["school-leader@example.com"])
+      expect(InviteParticipants).to have_received(:run).with(["rp-ect-ambition@example.com"])
+    end
+
+    it "does not create an invite email for old mentors and early career teachers" do
+      described_class.perform
+
+      allow(InviteParticipants).to receive(:run)
+      described_class.perform
+      expect(InviteParticipants).not_to have_received(:run)
     end
   end
 end
