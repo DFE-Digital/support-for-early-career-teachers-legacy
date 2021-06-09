72e965a5219dfe056689b82cc7d43399eee4c266
Michal Kreft <michal.kreft@softwire.com>
2021-06-09 12:19:45 +0100

    Review markups

--- app/controllers/core_induction_programmes/lessons_controller.rb
+++ app/controllers/core_induction_programmes/lessons_controller.rb
@@ -62,7 +62,15 @@ private
 
   def course_lesson_params
     params.require(:course_lesson).permit(
-      :title, :mentor_title, :ect_summary, :mentor_summary, :completion_time_in_minutes, :course_module_id, :new_position, :ect_teacher_standards, :mentor_teacher_standards
+      :title,
+      :mentor_title,
+      :ect_teacher_standards,
+      :mentor_teacher_standards,
+      :ect_summary,
+      :mentor_summary,
+      :completion_time_in_minutes,
+      :course_module_id,
+      :new_position,
     )
   end
 end
