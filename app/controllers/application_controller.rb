33fb4c62603ac728ba26d00e0e72e92877382168
Michal Kreft <michal.kreft@softwire.com>
2021-06-14 15:47:07 +0100

    Add sentry - without credentials yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   default_form_builder GOVUKDesignSystemFormBuilder::FormBuilder
 
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_sentry_user, except: :check, unless: :devise_controller?
 
   def check
     render json: { status: "OK", version: release_version, sha: ENV["SHA"], environment: Rails.env }, status: :ok
@@ -25,5 +26,11 @@ protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[email full_name])
+  end
+
+  def set_sentry_user
+    return if current_user.blank?
+
+    Sentry.set_user(id: current_user.id)
   end
 end
