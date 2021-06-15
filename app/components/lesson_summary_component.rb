62d459a11aec9de64f7824f53819c0f2b9c62ea0
Michal Kreft <michal.kreft@softwire.com>
2021-06-15 16:36:07 +0100

    Use component to improve readability

--- app/components/lesson_summary_component.rb
+++ app/components/lesson_summary_component.rb
@@ -2,7 +2,11 @@
 
 class LessonSummaryComponent < ViewComponent::Base
   def initialize(lesson:, user:)
-    @lesson = lesson
     @user = user
+    @lesson = lesson
+    @mentor_materials = @lesson.mentor_materials
+    @lesson_title = @lesson.title_for(@user)
+    @lesson_has_self_study = @lesson.course_lesson_parts.any?
+    @teacher_standards = @lesson.teacher_standards_for(@user)
   end
 end
