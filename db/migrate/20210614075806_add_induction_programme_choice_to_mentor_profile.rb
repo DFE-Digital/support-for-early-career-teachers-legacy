6b1edac29fe469eafa0c9a637bd5c7ad16b4e37e
xswoodsx <steven.woods@digital.education.gov.uk>
2021-06-15 17:00:19 +0100

    Add induction_programme_choice to ect and mentor profiles

---
+++ db/migrate/20210614075806_add_induction_programme_choice_to_mentor_profile.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+class AddInductionProgrammeChoiceToMentorProfile < ActiveRecord::Migration[6.1]
+  def change
+    add_column :mentor_profiles, :induction_programme_choice, :string
+  end
+end
