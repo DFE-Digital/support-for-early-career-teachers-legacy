6c31be75c3242c9a2bdcefb29b994e59735e3aae
matt <mattonfoot@gmail.com>
2021-06-16 20:13:27 +0100

    forgot cypress path config

--- spec/cypress/support/step_definitions/common-navigation.js
+++ spec/cypress/support/step_definitions/common-navigation.js
@@ -5,6 +5,7 @@ Given("scenario {string} has been ran", (scenario) => cy.appScenario(scenario));
 const pagePaths = {
   cookie: "/cookies",
   start: "/",
+  accessibility: "/accessibility-statement",
   dashboard: "/dashboard",
   "edit preferred name": "/preferred-name/edit",
   "sign in": "/users/sign_in",
