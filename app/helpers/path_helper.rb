dbf92425805b952be3131837410fec762b455281
Callum Macrae <callum.macrae@digital.education.gov.uk>
2021-07-01 11:54:13 +0100

    Metaprogramme away most of the horribleness of the path helper

--- app/helpers/path_helper.rb
+++ app/helpers/path_helper.rb
@@ -1,77 +1,77 @@
 # frozen_string_literal: true
 
-# rubocop:disable Rails/HelperInstanceVariable
+# This helper metaprograms in a bunch of path helper methods that went away when
+# we switched to using nested resources that we wanted to keep for convenience
+# and for brevity. Effectively what it does is define methods like this:
+#
+#   def module_path(course_module = @course_module)
+#     cip_year_module_path(course_module.course_year.core_induction_programme, course_module.course_year, course_module)
+#   end
+#
+# That's just for module paths, which are three levels deep - mentor material
+# parts are six levels deep, so as you can image get pretty messy!
 module PathHelper
   def years_path(*args)
     cip_years_path(*args)
   end
 
-  def year_path(course_year = @course_year, *args)
-    cip_year_path(course_year.core_induction_programme, course_year, *args)
+  models = {
+    cip: "core_induction_programme",
+    year: "course_year",
+    module: "course_module",
+    lesson: "course_lesson",
+    lesson_part: "course_lesson_part",
+    mentor_material: "mentor_material",
+    mentor_material_part: "mentor_material_part",
+  }
+
+  parents = {
+    year: "cip",
+    module: "year",
+    lesson: "module",
+    lesson_part: "lesson",
+    mentor_material: "lesson",
+    mentor_material_part: "mentor_material",
+  }
+
+  get_name = lambda do |name, helper_obj|
+    to_prepend = helper_obj.key?(:prepend) ? helper_obj[:prepend] + "_" : ""
+    to_append = helper_obj.key?(:append) ? "_" + helper_obj[:append] : ""
+    "#{to_prepend}#{name}#{to_append}_path"
   end
 
-  def edit_year_path(course_year = @course_year, *args)
-    edit_cip_year_path(course_year.core_induction_programme, course_year, *args)
-  end
+  [
+    { name: "year" },
+    { name: "year", prepend: "edit" },
+    { name: "module" },
+    { name: "module", prepend: "edit" },
+    { name: "lesson" },
+    { name: "lesson", prepend: "edit" },
+    { name: "lesson_part" },
+    { name: "lesson_part", prepend: "edit" },
+    { name: "lesson_part", append: "split" },
+    { name: "lesson_part", append: "show_delete" },
+    { name: "lesson_part", append: "update_progress" },
+    { name: "mentor_material" },
+    { name: "mentor_material", prepend: "edit" },
+    { name: "mentor_material_part" },
+    { name: "mentor_material_part", prepend: "edit" },
+    { name: "mentor_material_part", append: "split" },
+    { name: "mentor_material_part", append: "show_delete" },
+  ].each do |helper|
+    define_method get_name.call(helper[:name], helper) do |resource = nil|
+      # use instance variable if nothing passed in - mostly done by forms
+      resource ||= instance_variable_get("@#{models[helper[:name].to_sym]}")
 
-  def module_path(course_module = @course_module, *args)
-    cip_year_module_path(course_module.course_year.core_induction_programme, course_module.course_year, course_module, *args)
-  end
+      # builds an array from resource name like ["module", "year", "cip"]
+      parts = [helper[:name]]
+      parts.push(parents[parts.last.to_sym]) while parents[parts.last.to_sym]
 
-  def edit_module_path(course_module = @course_module, *args)
-    edit_cip_year_module_path(course_module.course_year.core_induction_programme, course_module.course_year, course_module, *args)
-  end
+      # builds arguments from resource like [module, module.year, module.year.cip]
+      args = [resource]
+      parts[1..].each { |part| args.unshift(args.first.public_send(models[part.to_sym])) }
 
-  def lesson_path(course_lesson = @course_lesson, *args)
-    cip_year_module_lesson_path(course_lesson.course_module.course_year.core_induction_programme, course_lesson.course_module.course_year, course_lesson.course_module, course_lesson, *args)
-  end
-
-  def edit_lesson_path(course_lesson = @course_lesson, *args)
-    edit_cip_year_module_lesson_path(course_lesson.course_module.course_year.core_induction_programme, course_lesson.course_module.course_year, course_lesson.course_module, course_lesson, *args)
-  end
-
-  def lesson_part_path(lesson_part = @course_lesson_part, *args)
-    cip_year_module_lesson_lesson_part_path(lesson_part.course_lesson.course_module.course_year.core_induction_programme, lesson_part.course_lesson.course_module.course_year, lesson_part.course_lesson.course_module, lesson_part.course_lesson, lesson_part, *args)
-  end
-
-  def edit_lesson_part_path(lesson_part = @course_lesson_part, *args)
-    edit_cip_year_module_lesson_lesson_part_path(lesson_part.course_lesson.course_module.course_year.core_induction_programme, lesson_part.course_lesson.course_module.course_year, lesson_part.course_lesson.course_module, lesson_part.course_lesson, lesson_part, *args)
-  end
-
-  def lesson_part_split_path(lesson_part = @course_lesson_part, *args)
-    cip_year_module_lesson_lesson_part_split_path(lesson_part.course_lesson.course_module.course_year.core_induction_programme, lesson_part.course_lesson.course_module.course_year, lesson_part.course_lesson.course_module, lesson_part.course_lesson, lesson_part, *args)
-  end
-
-  def lesson_part_show_delete_path(lesson_part = @course_lesson_part, *args)
-    cip_year_module_lesson_lesson_part_show_delete_path(lesson_part.course_lesson.course_module.course_year.core_induction_programme, lesson_part.course_lesson.course_module.course_year, lesson_part.course_lesson.course_module, lesson_part.course_lesson, lesson_part, *args)
-  end
-
-  def lesson_part_update_progress_path(lesson_part = @course_lesson_part, *args)
-    cip_year_module_lesson_lesson_part_update_progress_path(lesson_part.course_lesson.course_module.course_year.core_induction_programme, lesson_part.course_lesson.course_module.course_year, lesson_part.course_lesson.course_module, lesson_part.course_lesson, lesson_part, *args)
-  end
-
-  def mentor_material_path(mentor_material = @mentor_material, *args)
-    cip_year_module_lesson_mentor_material_path(mentor_material.course_lesson.course_module.course_year.core_induction_programme, mentor_material.course_lesson.course_module.course_year, mentor_material.course_lesson.course_module, mentor_material.course_lesson, mentor_material, *args)
-  end
-
-  def edit_mentor_material_path(mentor_material = @mentor_material, *args)
-    edit_cip_year_module_lesson_mentor_material_path(mentor_material.course_lesson.course_module.course_year.core_induction_programme, mentor_material.course_lesson.course_module.course_year, mentor_material.course_lesson.course_module, mentor_material.course_lesson, mentor_material, *args)
-  end
-
-  def mentor_material_part_path(part = @mentor_material_part, *args)
-    cip_year_module_lesson_mentor_material_mentor_material_part_path(part.mentor_material.course_lesson.course_module.course_year.core_induction_programme, part.mentor_material.course_lesson.course_module.course_year, part.mentor_material.course_lesson.course_module, part.mentor_material.course_lesson, part.mentor_material, part, *args)
-  end
-
-  def edit_mentor_material_part_path(part = @mentor_material_part, *args)
-    edit_cip_year_module_lesson_mentor_material_mentor_material_part_path(part.mentor_material.course_lesson.course_module.course_year.core_induction_programme, part.mentor_material.course_lesson.course_module.course_year, part.mentor_material.course_lesson.course_module, part.mentor_material.course_lesson, part.mentor_material, part, *args)
-  end
-
-  def mentor_material_part_split_path(part = @mentor_material_part, *args)
-    cip_year_module_lesson_mentor_material_mentor_material_part_split_path(part.mentor_material.course_lesson.course_module.course_year.core_induction_programme, part.mentor_material.course_lesson.course_module.course_year, part.mentor_material.course_lesson.course_module, part.mentor_material.course_lesson, part.mentor_material, part, *args)
-  end
-
-  def mentor_material_part_show_delete_path(part = @mentor_material_part, *args)
-    cip_year_module_lesson_mentor_material_mentor_material_part_show_delete_path(part.mentor_material.course_lesson.course_module.course_year.core_induction_programme, part.mentor_material.course_lesson.course_module.course_year, part.mentor_material.course_lesson.course_module, part.mentor_material.course_lesson, part.mentor_material, part, *args)
+      public_send(get_name.call(parts.reverse.join("_"), helper), *args)
+    end
   end
 end
-# rubocop:enable Rails/HelperInstanceVariable
