0e7f22cff4a7218d873a2863800cc1de5be4bf8f
Michal Kreft <michal.kreft@softwire.com>
2021-07-07 13:40:22 +0100

    Remove unused references from CIP to course year

--- app/models/core_induction_programme.rb
+++ app/models/core_induction_programme.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 class CoreInductionProgramme < ApplicationRecord
-  # TODO: Remove them after running the migration on all environments
-  belongs_to :course_year_one, class_name: "CourseYear", optional: true
-  belongs_to :course_year_two, class_name: "CourseYear", optional: true
-
   has_many :course_years, -> { order(position: :asc) }
   has_many :course_modules, through: :course_years
   has_many :course_lessons, through: :course_modules
