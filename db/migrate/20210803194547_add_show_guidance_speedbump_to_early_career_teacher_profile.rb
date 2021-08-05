6d5388b8e5818bf1cc6ee4bd84e3b012bf5c9314
Steven <steven.woods@digital.education.gov.uk>
2021-08-05 11:07:24 +0100

    PR feedback: Update migrations and schema

--- db/migrate/20210803194547_add_show_guidance_speedbump_to_early_career_teacher_profile.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddShowGuidanceSpeedbumpToEarlyCareerTeacherProfile < ActiveRecord::Migration[6.1]
-  def change
-    add_column :early_career_teacher_profiles, :show_guidance_speedbump, :boolean, default: true
-  end
-end
--- db/migrate/20210803194620_add_show_guidance_speedbump_to_mentor_profile.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddShowGuidanceSpeedbumpToMentorProfile < ActiveRecord::Migration[6.1]
-  def change
-    add_column :mentor_profiles, :show_guidance_speedbump, :boolean, default: true
-  end
-end
