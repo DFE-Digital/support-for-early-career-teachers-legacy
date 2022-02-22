fa5fdcd5d7750f1846990f98e50341d46ec39709
Jack Bracewell <jack@reasonable.engineering>
2022-02-22 17:50:23 +0000

    Fix linting

--- db/migrate/20220218105019_add_verification_fields_to_external_user_profiles.rb
+++ db/migrate/20220218105019_add_verification_fields_to_external_user_profiles.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class AddVerificationFieldsToExternalUserProfiles < ActiveRecord::Migration[6.1]
   def change
-    add_column :external_user_profiles, :verification_token, :string
-    add_column :external_user_profiles, :verification_expires_at, :datetime
-    add_column :external_user_profiles, :verified, :boolean, default: false
+    change_table :external_user_profiles, bulk: true do
+      add_column :external_user_profiles, :verification_token, :string
+      add_column :external_user_profiles, :verification_expires_at, :datetime
+      add_column :external_user_profiles, :verified, :boolean, default: false
+    end
   end
 end
